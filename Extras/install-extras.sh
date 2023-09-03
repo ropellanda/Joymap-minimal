d934011d0a71d130b9f77167e58b90db93b64cf7
ropellanda <ropellanda@gmail.com>
2023-09-03 11:03:15 -0300

    Update install-extras.sh

--- Extras/install-extras.sh
+++ Extras/install-extras.sh
@@ -1,2 +1,4 @@
 sudo cp -R /home/pi/joymap-minimal/Extras/dark-switch /etc/emulationstation/themes/
 sudo cp -R /home/pi/joymap-minimal/Extras/light-switch /etc/emulationstation/themes/
+sudo cp /home/pi/joymap-minimal/SwitchBerry.jpg /home/pi/RetroPie/splashscreens/SwitchBerry.jpg
+sudo cp /home/pi/joymap-minimal/splashscreen.list /etc/splashscreen.list
